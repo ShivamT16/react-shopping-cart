0d31c3e960ca879171993a25b3fde3794b22978d
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-11-30 19:10:59 +0530

    fix: Enhancement in Mobile

--- src/Pages/CategoryWise.js
+++ src/Pages/CategoryWise.js
@@ -4,10 +4,11 @@ import { ToastContainer } from "react-toastify";
 import products from "../Data/products.json"
 import { ProductContext } from "../Contexts/ProductContext";
 import { CartContext } from "../Contexts/CartContext";
+import { ProductDetail } from "./ProductDetail";
 
 export const CategoryWise = () => {
   const { cartNotify, state, dispatch } = useContext(CartContext);
-  const { handleProduct, productCategory } = useContext(ProductContext);
+  const { handleProduct, productCategory, handleOpen } = useContext(ProductContext);
  
   return (
     <div className="bg-[whitesmoke] mt-16 flex flex-wrap gap-4 pb-20 pt-8 items-center justify-center">
@@ -21,11 +22,11 @@ export const CategoryWise = () => {
               className="border-[1.9px] border-[#514a9d] p-4 rounded-[20px]"
               onClick={() => handleProduct(id)}
             >
-              <Link className="link" to="/productDetail">
+              <div>
                 <img className="h-64 w-60 border border-[#24c6dc] rounded-2xl p-4 bg-white" alt="product img" src={image} />
                 <ul>{name} </ul>
                 <p> INR: {price}</p>
-              </Link>
+              </div>
               {state.cart.find((element) => element.id === item.id) ? (
                 <Link to="/cart" className="border-2 border-[#514a9d] px-2.5 rounded-lg bg-white hover:bg-[lightgray]">
                  Go to Cart
@@ -41,11 +42,12 @@ export const CategoryWise = () => {
                   Add to Cart
                 </Link>
               )}
+             <Link className="border-2 border-[#514a9d] mx-2 py-0.5 px-1 rounded-lg bg-white hover:bg-[lightgray]" onClick={() => {handleProduct(id); handleOpen()}} > View Details </Link>
               
             </div>
           );
         })}
-
+        <ProductDetail />
         <ToastContainer autoClose={1500} />
     </div>
   );
--- src/Pages/ProductDetail.js
+++ src/Pages/ProductDetail.js
@@ -15,27 +15,29 @@ export const ProductDetail = () => {
   return (
     <div>
      <Modal open={open} onClose={handleClose} >
-      <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[1400px]" >
+      <Box className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[450px] sm:w-[800px] md:w-[1300px]" >
       {products.data.filter(({ id }) => id === product).map((item) => {
           const { id, name, price, description, image } = item;
           return (
-            <div className="detaill">
+            <div>
               <div key={id} className="border border-[#514a9d] bg-[whitesmoke] rounded-2xl flex flex-wrap p-8 m-8 mx-16">
+                
+                  <img className="h-26 w-20 border border-[#24c6dc] rounded-2xl p-1 bg-white md:w-52" alt="product img" src={image} />
                 <div>
-                  <img className="h-60 w-54 border border-[#24c6dc] rounded-2xl p-4 bg-white" alt="product img" src={image} />
-                  <ul className="pname">{name}</ul>
+                  <p className="text-lg font-medium">{name}</p>
                   <p>INR: {price}</p>
                 </div>
-                <div className="w-[50rem] text-justify p-6 m-4">
-                  <p className="mb-2"> {description} </p>
+                
+                <div className="w-[40rem] text-justify p-0 m-0 md:p-2 m-4">
+                  <p className="text-sm mb-0 md:mb-2 md:text-base"> {description} </p>
 
                 {state.cart.find((element) => element.id === item.id) ? (
-                <Link to="/cart" className="border-2 border-[#514a9d] px-2.5 rounded-lg bg-white hover:bg-[lightgray]">
+                <Link to="/cart" className="border-2 border-lime-600 px-2 rounded-lg bg-lime-500 hover:bg-[lightgray]">
                  Go to Cart
                 </Link>
               ) : (
                 <Link
-                  className="border-2 border-[#514a9d] px-2.5 rounded-lg bg-white hover:bg-[lightgray]"
+                  className="border-2 border-[#514a9d] px-2 text-sm rounded-lg bg-white hover:bg-[lightgray]"
                   onClick={() => {
                     dispatch({type:"ADD_TO_CART", payload: item})
                     cartNotify();
@@ -47,7 +49,7 @@ export const ProductDetail = () => {
 
               <br />
                <button onClick={handleClose}
-               className="border-2 border-slate-500 mt-4 px-2.5 rounded-lg bg-white hover:bg-[lightgray]"
+               className="border-2 border-slate-500 mt-1 px-2 text-sm rounded-lg bg-white hover:bg-[lightgray]"
                > Close </button>                  
                 </div>
               </div>
